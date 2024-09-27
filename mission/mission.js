88a66d817b9f6d600e772d0e9a3121efc59953fc
Luke Frandsen <168469565+LukeFrandsen@users.noreply.github.com>
2024-09-27 12:39:12 -0600

    dark theme

--- mission/mission.js
+++ mission/mission.js
@@ -1,11 +1,14 @@
 const themeSelect = document.getElementById('theme');
+const logo = document.getElementById('logo');
+
 themeSelect.addEventListener('change', function() {
     const body = document.body;
     if (themeSelect.value === 'dark') {
         body.classList.add('dark');
+        logo.src = 'images/byui-logo_dark.png';
     }
     else {
         body.classList.remove('dark');
-        logo.src = 'byui-logo_blue.webp';
+        logo.src = 'images/byui-logo_blue.webp';
     }
 });
